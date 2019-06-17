200dd22b371c30db339cb259986f4f9a49fadc6e
MichalAW <michal.wantaeuco@gmail.com>
2019-06-17 18:46:44 +0200

    function component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import Todo from './Todo';
 
-class TodoList extends React.Component {
-    render() {
-        return(
-            <div>
-                {this.props.data.map((todo, index) => {
-                    return(
-                        <Todo key={todo} text={todo.text} id={todo.id}/>
-                    )
-                })}
-            </div>
-        )
-    }
-}
+const TodoList = props => <div>{props.data.map(todo => <Todo key={todo.id} text={todo.text} id={todo.id} removeTodo={props.removeTodo}/>)}</div>
 
 export default TodoList;
